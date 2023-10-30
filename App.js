c3698008952a25c1c94b480f7be0b2d1e32c95a1
Patnana-Harsha <harsha.patnana330@gmail.com>
2023-10-30 20:22:31 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},[
+React.createElement("h1",{}, "I'm H1"),
+React.createElement("h2",{}, "I'm H2")
+]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
